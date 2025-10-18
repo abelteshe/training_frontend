29ccacad629230999b8b1ca9c5d72ab9dacb4d3e
Abel Teshome <abelteshe@gmail.com>
2025-10-18 16:08:13 +0300

    first commit

---
+++ src/views/Set.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+    <main>
+        <TheWelcome />
+    </main>
+</template>
+<style>
+@media (min-width: 1024px) {
+    main {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
